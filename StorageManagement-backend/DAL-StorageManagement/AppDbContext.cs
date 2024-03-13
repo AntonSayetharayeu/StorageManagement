84c569ae6177cd9de9a8e58e202e49e6e148a120
AntonSayetharayeu <antonsayet@gmail.com>
2024-03-13 22:33:13 +0100

    [New Feature] Application architecture improvement

---
+++ StorageManagement-backend/DAL-StorageManagement/AppDbContext.cs
@@ -0,0 +1,23 @@
+﻿using DAL_StorageManagement.Model;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL_StorageManagement
+{
+    public class AppDbContext :  DbContext 
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Role> Roles { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Order> Orders { get; set; }
+
+        
+        
+    }
+}
---
+++ StorageManagement-backend/DAL-StorageManagement/Model/Order.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL_StorageManagement.Model
+{
+    public class Order
+    {
+        public int ID { get; set; }
+        public DateTime Date { get; set; }
+        public int UserID { get; set; }
+        public int Amount { get; set; }
+
+        //Navigation
+        public User User { get; set; }
+        public List<Product> Products { get; set; }
+    }
+}
